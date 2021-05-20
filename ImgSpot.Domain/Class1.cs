f260fca521db27319351869ffc9ebe0ee25da0ec
chris young <christopher.young@revature.net>
2021-05-20 11:38:18 -0400

    service structure

---
+++ ImgSpot.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ImgSpot.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ ImgSpot.Storage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ImgSpot.Storage
+{
+    public class Class1
+    {
+    }
+}
