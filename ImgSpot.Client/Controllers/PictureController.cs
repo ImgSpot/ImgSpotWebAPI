c0091deefd4b0e29dce4bdbbd0bb88385024bb7f
chris young <christopher.young@revature.net>
2021-05-26 00:42:38 -0400

    Initialize Controllers

---
+++ ImgSpot.Client/Controllers/PictureController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImgSpot.Client.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  []
+  public class PictureController : ControllerBase
+  {
+    [HttpGet]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    /*     public IActionResult Get()
+        {
+
+        } */
+  }
+}
