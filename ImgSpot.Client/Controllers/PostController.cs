62626a9a1e8fa2d1f7721a798ccfc144b7f6e9fc
chris young <christopher.young@revature.net>
2021-05-27 20:13:14 -0400

    Switch to Newtonsoft.Json null handling

--- ImgSpot.Client/Controllers/PostController.cs
+++ ImgSpot.Client/Controllers/PostController.cs
@@ -1,9 +1,10 @@
 using System.Linq;
-using System.Text.Json;
+//using System.Text.Json;
 using ImgSpot.Client.Models;
 using ImgSpot.Storage;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ImgSpot.Client.Controllers
 {
@@ -24,10 +25,15 @@ namespace ImgSpot.Client.Controllers
     //[ValidateAntiForgeryToken]
     public IActionResult Get()
     {
+      var settings = new JsonSerializerSettings
+      {
+        NullValueHandling = NullValueHandling.Ignore,
+        MissingMemberHandling = MissingMemberHandling.Ignore
+      };
       //return likes, comments, pictures
       var post = new PostModel();
       post.Load(_unitOfWork);
-      string jsonString = JsonSerializer.Serialize(post);
+      string jsonString = JsonConvert.SerializeObject(post, settings);
       //serialize into json string
       //return Ok( serialized json string );
       return Ok(jsonString);
@@ -37,16 +43,23 @@ namespace ImgSpot.Client.Controllers
     //[ValidateAntiForgeryToken]
     public IActionResult Post(string jsonString)
     {
+      var settings = new JsonSerializerSettings
+      {
+        NullValueHandling = NullValueHandling.Ignore,
+        MissingMemberHandling = MissingMemberHandling.Ignore
+      };
       var post = new PostModel();
-      post = JsonSerializer.Deserialize<PostModel>(jsonString);
+      post = JsonConvert.DeserializeObject<PostModel>(jsonString, settings);
       //post.Load(_unitOfWork);
-      var user = _unitOfWork.Users.Select(c => c.Username == post.SelectedUser).First();
-      var picture = _unitOfWork.Pictures.Select(c => (c.Username == post.SelectedPicture) && (c.Filename == post.SelectedPicture)).First();
-      var comment = _unitOfWork.Comments.Select(c => (c.Username == post.SelectedComment) && (c.Filename == post.SelectedComment)).First();
+      /*
+     var user = _unitOfWork.Users.Select(c => c.Username == post.SelectedUser).First();
+     var picture = _unitOfWork.Pictures.Select(c => (c.Username == post.SelectedPicture) && (c.Filename == post.SelectedPicture)).First();
+     var comment = _unitOfWork.Comments.Select(c => (c.Username == post.SelectedComment) && (c.Filename == post.SelectedComment)).First();
 
-      _unitOfWork.Users.Insert(user);
-      _unitOfWork.Pictures.Insert(picture);
-      _unitOfWork.Comments.Insert(comment);
+     _unitOfWork.Users.Insert(user);
+     _unitOfWork.Pictures.Insert(picture);
+     _unitOfWork.Comments.Insert(comment); */
+
       _unitOfWork.Save();
       return Ok(post);
     }
---
+++ ImgSpot.Client/ImagetoJson.cs
@@ -0,0 +1,14 @@
+/* using System;
+using System.Text.Json.Serialization;
+
+namespace ImgSpot.Client
+{
+  public class ImagetoJson : JsonConverter
+  {
+    public override bool CanConvert(Type objectType)
+    {
+      return objectType == typeof(string);
+    }
+  }
+}
+ */
